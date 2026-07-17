3ab246604dfc6795ffa01d202f2042473b2152be
Vitor Sampaio <vitorsampaio.tds2.senai@gmail.com>
2026-07-17 01:16:15 -0300

    Corrige base do Vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
+  base: '/site_her/',
+  server: {
+    port: 5173
+  }
 })
